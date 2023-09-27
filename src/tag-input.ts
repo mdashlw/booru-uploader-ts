26a0cb71cd36d1999ac5f1016d0799e2993ba225
mdashlw <mdashlw@gmail.com>
2023-09-27 15:52:05 +0300

    refactor(tag-input): new inquirer

--- src/tag-input.ts
+++ src/tag-input.ts
@@ -1,90 +1,83 @@
-import inquirer, { Answers, ChoiceCollection } from "inquirer";
-import inquirerPrompt from "inquirer-autocomplete-prompt";
+import { Separator } from "@inquirer/prompts";
+import type { ChoiceOrSeparatorArray } from "inquirer-autocomplete-standalone";
+import autocomplete from "inquirer-autocomplete-standalone";
 import TagLists from "./booru/tag-lists.js";
+import { TagName } from "./booru/types.js";
 import { debounceAsync } from "./lodash.js";
-
-inquirer.registerPrompt("autocomplete", inquirerPrompt);
 
 export default async function inputTags(tags: TagLists) {
   while (true) {
     console.log();
     await tags.printAll();
 
-    const { tagInput } = await inquirer.prompt([
-      {
-        type: "autocomplete",
-        name: "tagInput",
-        message: "Tags...",
-        source: debounceAsync(
-          async (
-            _: Answers,
-            term: string | null,
-          ): Promise<ChoiceCollection> => {
-            if (!term) {
-              return [];
-            }
+    const tagInput = await autocomplete({
+      message: "Tags...",
+      source: debounceAsync(
+        async (
+          input: string | undefined,
+        ): Promise<ChoiceOrSeparatorArray<string>> => {
+          if (!input) {
+            return [];
+          }
 
-            let prefix = "";
+          let prefix = "";
 
-            if (term.includes(",")) {
-              const lastCommaIndex = term.lastIndexOf(",");
-              const newTerm = term.substring(lastCommaIndex + 1);
-              const trimmedNewTerm = newTerm.trimStart();
+          if (input.includes(",")) {
+            const lastCommaIndex = input.lastIndexOf(",");
+            const newTerm = input.substring(lastCommaIndex + 1);
+            const trimmedNewTerm = newTerm.trimStart();
 
-              prefix = term.substring(
-                0,
-                lastCommaIndex + 1 + (newTerm.length - trimmedNewTerm.length),
-              );
-              term = trimmedNewTerm;
-            } else {
-              term = term.trimStart();
-            }
+            prefix = input.substring(
+              0,
+              lastCommaIndex + 1 + (newTerm.length - trimmedNewTerm.length),
+            );
+            input = trimmedNewTerm;
+          } else {
+            input = input.trimStart();
+          }
 
-            if (term.startsWith("-")) {
-              prefix += "-";
-              term = term.substring(1);
-            }
+          if (input.startsWith("-")) {
+            prefix += "-";
+            input = input.substring(1);
+          }
 
-            if (!term) {
-              return [];
-            }
+          if (!input) {
+            return [];
+          }
 
-            return (
-              await Promise.all(
-                tags.boorus.map(async (booru, index, boorus) => {
-                  const tags = await booru
-                    .autocompleteTags(term!)
-                    .catch((error) => {
-                      console.error(
-                        new Error(
-                          `Failed to autocomplete tags for "${term}" on ${booru.name}`,
-                          { cause: error },
-                        ),
-                      );
-                      return [];
-                    });
-                  const choices: ChoiceCollection = tags.map(
-                    ({ label, value }) => ({
-                      name: label,
-                      value: prefix + value,
-                    }),
-                  );
+          return (
+            await Promise.all(
+              tags.boorus.map(async (booru, index, boorus) => {
+                const tags = await booru
+                  .autocompleteTags(input!)
+                  .catch((error) => {
+                    console.error(
+                      new Error(
+                        `Failed to autocomplete tags for "${input}" on ${booru.name}`,
+                        { cause: error },
+                      ),
+                    );
+                    return [];
+                  });
+                const choices: any = tags.map(({ label, value }) => ({
+                  name: label,
+                  value: prefix + value,
+                }));
 
-                  if (index !== boorus.length - 1) {
-                    choices.push(new inquirer.Separator());
-                  }
+                if (index !== boorus.length - 1) {
+                  choices.push(new Separator());
+                }
 
-                  return choices;
-                }),
-              )
-            ).flat();
-          },
-          100,
-        ),
-        pageSize: Infinity,
-        suggestOnly: true,
-      },
-    ]);
+                return choices;
+              }),
+            )
+          ).flat();
+        },
+        100,
+      ),
+      pageSize: Infinity,
+      suggestOnly: true,
+    });
 
     if (!tagInput) {
       break;
@@ -104,11 +97,11 @@ export default async function inputTags(tags: TagLists) {
           continue;
         }
 
-        await tags.removeByName(tagName);
+        await tags.removeByName(tagName as TagName);
         continue;
       }
 
-      await tags.addByName(tagName);
+      await tags.addByName(tagName as TagName);
     }
   }
 }
