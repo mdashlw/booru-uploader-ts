db7406b2a0d20cb4807cd967216055f316860bc9
mdashlw <mdashlw@gmail.com>
2023-11-29 17:05:25 +0300

    types(utils): specify type

--- src/utils/stream.ts
+++ src/utils/stream.ts
@@ -2,7 +2,7 @@ import { Buffer } from "node:buffer";
 import { Readable } from "node:stream";
 
 export async function readableToBuffer(readable: Readable): Promise<Buffer> {
-  const chunks = [];
+  const chunks: Buffer[] = [];
 
   for await (const chunk of readable) {
     chunks.push(chunk);
