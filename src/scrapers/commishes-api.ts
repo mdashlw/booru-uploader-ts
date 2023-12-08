2b732076e26da1082742f527f85eebc6204fe4c4
mdashlw <mdashlw@gmail.com>
2023-12-08 09:23:48 +0300

    feat(commishes-api): fetch started timestamp, canonical url

--- src/scrapers/commishes-api.ts
+++ src/scrapers/commishes-api.ts
@@ -29,17 +29,24 @@ const Auction = z.object({
   username: z.string(),
   title: z.string().nullable(),
   description: z.string().nullable(),
-  endsunix: z
-    .number()
-    .int()
-    .positive()
-    .transform((ts) => ts * 1_000)
-    .pipe(z.coerce.date()),
   media: z.object({
     original: z.string().url(),
   }),
 });
 type Auction = z.infer<typeof Auction>;
+
+const AuctionExtended = z.object({
+  id: z.number().int().positive(),
+  url: z.string().startsWith("/"),
+  userName: z.string(),
+  started: z
+    .number()
+    .int()
+    .positive()
+    .transform((ts) => ts * 1_000)
+    .pipe(z.coerce.date()),
+});
+type AuctionExtended = z.infer<typeof AuctionExtended>;
 
 export function canHandle(url: URL): boolean {
   return (
@@ -55,13 +62,17 @@ export async function scrape(url: URL): Promise<SourceData> {
 
   if (url.hostname === "ych.commishes.com") {
     const auction = await fetchAuction(objectId);
+    const auctionExtended = await fetchAuctionExtended(
+      auction.username,
+      auction.id,
+    );
     const { type, width, height } = await probeImageSize(
       auction.media.original,
     );
 
     return {
       source: "Commishes",
-      url: `https://${url.host}/auction/show/${objectId}/`,
+      url: `https://${url.host}${auctionExtended.url}`,
       images: [
         {
           url: auction.media.original,
@@ -71,7 +82,7 @@ export async function scrape(url: URL): Promise<SourceData> {
         },
       ],
       artist: auction.username,
-      date: `(Ends) ${formatDate(auction.endsunix)}`,
+      date: formatDate(auctionExtended.started),
       title: auction.title,
       description: auction.description?.replaceAll("\r\n", "\n") ?? null,
     };
@@ -144,4 +155,43 @@ async function fetchAuction(auctionId: string): Promise<Auction> {
     .parse(json);
 
   return data.payload;
+}
+
+async function fetchAuctionExtended(
+  username: string,
+  auctionId: number,
+): Promise<AuctionExtended> {
+  const response = await undici
+    .request(
+      `https://ych.commishes.com/user/history/${username}.json?until=${
+        auctionId + 1
+      }&rating=100`,
+      {
+        throwOnError: true,
+      },
+    )
+    .catch((error) => {
+      error = new Error("Failed to fetch", { cause: error });
+      error.auctionId = auctionId;
+      throw error;
+    });
+  const json = await response.body.json().catch((error) => {
+    error = new Error("Failed to read response body", { cause: error });
+    error.auctionId = auctionId;
+    error.response = response;
+    throw error;
+  });
+  const data = z
+    .object({
+      status: z.literal("200 OK"),
+      payload: AuctionExtended.array(),
+    })
+    .parse(json);
+  const auction = data.payload.find((a) => a.id === auctionId);
+
+  if (!auction) {
+    throw new Error("Auction not found");
+  }
+
+  return auction;
 }
