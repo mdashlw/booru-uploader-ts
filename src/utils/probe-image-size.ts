7bbf2f8c31db883d3153ca1fe486ab6892994bc1
mdashlw <mdashlw@gmail.com>
2023-12-24 11:31:19 +0300

    fix(utils/probe-image-size): fix if path starts with "//"

--- src/utils/probe-image-size.ts
+++ src/utils/probe-image-size.ts
@@ -4,6 +4,14 @@ import undici from "undici";
 export default async function probeImageSize(
   url: string | URL,
 ): Promise<probe.ProbeResult> {
+  if (!(url instanceof URL)) {
+    url = new URL(url);
+  }
+
+  if (url.pathname.startsWith("//")) {
+    url.pathname = url.pathname.substring(1);
+  }
+
   const ac = new AbortController();
   const response = await undici.request(url, {
     signal: ac.signal,
