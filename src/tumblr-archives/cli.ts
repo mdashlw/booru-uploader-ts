018329ebf06ba1fcf78f8986144a605c491d84d6
mdashlw <mdashlw@gmail.com>
2024-03-04 17:03:26 +0300

    feat(tumblr-archives/cli): new command "reblogs" to show reblogs for post

--- src/tumblr-archives/cli.ts
+++ src/tumblr-archives/cli.ts
@@ -1,12 +1,13 @@
 import process from "node:process";
 import util from "node:util";
-import { archivePosts } from "./api.js";
+import { archivePosts, getReblogs } from "./api.js";
 
 const [, , command] = process.argv;
 
 if (!command) {
   console.error("Usage:");
   console.error("- cli.ts archive --blog <blog name>");
+  console.error("- cli.ts reblogs --post <post id>");
   process.exit(1);
 }
 
@@ -26,6 +27,39 @@ if (command === "archive") {
   }
 
   await archivePosts(args.blog);
+} else if (command === "reblogs") {
+  const { values: args } = util.parseArgs({
+    args: process.argv.slice(3),
+    options: {
+      post: {
+        type: "string",
+      },
+    },
+  });
+
+  if (!args.post) {
+    console.error("Invalid usage: missing --post <post id>");
+    process.exit(1);
+  }
+
+  const postId = args.post.split("/").find((s) => /^\d+$/.test(s));
+
+  if (!postId) {
+    console.error("Invalid post id");
+    process.exit(1);
+  }
+
+  const reblogs = await getReblogs(postId);
+
+  if (reblogs) {
+    for (const reblog of reblogs) {
+      console.log(
+        `Post ${reblog.rebloggedRootId} by ${reblog.rebloggedRootName} at ${reblog.rebloggedRootUrl} reblogged by ${reblog.blogName} at ${reblog.postUrl}`,
+      );
+    }
+  } else {
+    console.log("No reblogs");
+  }
 } else {
   console.error("Unknown command");
   process.exit(1);
