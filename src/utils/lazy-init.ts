d27ce908b15921829cc30a060ef43281c084fe7f
mdashlw <mdashlw@gmail.com>
2024-03-13 07:28:23 +0300

    feat(utils/lazy-init): async only; support ttl

--- src/utils/lazy-init.ts
+++ src/utils/lazy-init.ts
@@ -1,4 +1,29 @@
-export const lazyInit = <T, Args extends any[]>(fn: (...args: Args) => T) => {
+import { setTimeout } from "node:timers";
+
+export const lazyInit = <T, Args extends any[]>(
+  fn: (...args: Args) => Promise<T | { value: T; ttlSeconds?: number }>,
+) => {
   let prom: T | undefined = undefined;
-  return (...args: Args) => (prom = prom ?? fn(...args));
+
+  return async (...args: Args) => {
+    if (prom !== undefined) {
+      return prom;
+    }
+
+    const ret = await fn(...args);
+
+    if (typeof ret === "object" && ret !== null && "value" in ret) {
+      prom = ret.value;
+
+      if (ret.ttlSeconds) {
+        setTimeout(() => {
+          prom = undefined;
+        }, ret.ttlSeconds * 1_000).unref();
+      }
+    } else {
+      prom = ret;
+    }
+
+    return prom;
+  };
 };
