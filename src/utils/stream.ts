098351c5fdc0fecd10d7e1c2f4e13be30d7499a7
mdashlw <mdashlw@gmail.com>
2024-04-25 13:41:17 +0300

    refactor: use new Array.fromAsync

--- src/utils/stream.ts
+++ src/utils/stream.ts
@@ -2,11 +2,5 @@ import { Buffer } from "node:buffer";
 import { Readable } from "node:stream";
 
 export async function readableToBuffer(readable: Readable): Promise<Buffer> {
-  const chunks: Buffer[] = [];
-
-  for await (const chunk of readable) {
-    chunks.push(chunk);
-  }
-
-  return Buffer.concat(chunks);
+  return Buffer.concat(await Array.fromAsync(readable));
 }
