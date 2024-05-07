01b86714485465bb2251c55ca0aa805fe50f06b8
mdashlw <mdashlw@gmail.com>
2024-05-07 20:25:03 +0300

    forgot to commit

---
+++ src/tags/autocomplete.ts
@@ -0,0 +1,27 @@
+import undici from "undici";
+import LocalAutocompleter from "./local-autocompleter.js";
+
+async function fetchLocalAutocomplete(): Promise<LocalAutocompleter> {
+  const now = new Date();
+  const cacheKey = `${now.getUTCFullYear()}-${now.getUTCMonth()}-${now.getUTCDate()}`;
+
+  const response = await undici.request(
+    `https://derpibooru.org/autocomplete/compiled?vsn=2&key=${cacheKey}`,
+    {
+      headers: { "user-agent": "" },
+      throwOnError: true,
+    },
+  );
+  const buffer = await response.body.arrayBuffer();
+
+  return new LocalAutocompleter(buffer);
+}
+
+export const autocompleter = await fetchLocalAutocomplete();
+
+export default function autocompleteTags(input: string): {
+  name: string;
+  imageCount: number;
+}[] {
+  return autocompleter.topK(input, 5);
+}
