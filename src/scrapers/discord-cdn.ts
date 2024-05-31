9967f5fcdc11d0d4c296c9e25340d4755256d83e
mdashlw <mdashlw@gmail.com>
2024-05-31 15:27:03 +0300

    feat(discord-cdn): refresh expired/unsigned attachment urls

--- src/scrapers/discord-cdn.ts
+++ src/scrapers/discord-cdn.ts
@@ -1,3 +1,4 @@
+import process from "node:process";
 import undici from "undici";
 import { SourceData } from "../scraper/types.js";
 import { formatDate } from "../scraper/utils.js";
@@ -8,23 +9,7 @@ export function canHandle(url: URL): boolean {
 }
 
 export async function scrape(url: URL): Promise<SourceData> {
-  const exString = url.searchParams.get("ex");
-
-  if (exString === null) {
-    throw new Error("unsigned url");
-  }
-
-  const exSeconds = Number.parseInt(exString, 16);
-
-  if (Number.isNaN(exSeconds)) {
-    throw new Error("invalid ex value in url");
-  }
-
-  const exMillis = exSeconds * 1_000;
-
-  if (Date.now() > exMillis) {
-    throw new Error("expired url");
-  }
+  url = await validateAndRefreshAttachmentUrl(url);
 
   const cleanUrl = new URL(url);
   cleanUrl.search = "";
@@ -49,4 +34,67 @@ export async function scrape(url: URL): Promise<SourceData> {
     title: null,
     description: null,
   };
+}
+
+async function validateAndRefreshAttachmentUrl(url: URL) {
+  const refresh = (errorMessage: string) =>
+    refreshAttachmentUrls([url.toString()]).then(
+      ([{ refreshed }]) => new URL(refreshed),
+      (error) => {
+        throw new Error(errorMessage, { cause: error });
+      },
+    );
+
+  const exString = url.searchParams.get("ex");
+
+  if (exString === null) {
+    return refresh("unsigned url");
+  }
+
+  const exSeconds = Number.parseInt(exString, 16);
+
+  if (Number.isNaN(exSeconds)) {
+    return refresh("invalid ex value in url");
+  }
+
+  const exMillis = exSeconds * 1_000;
+
+  if (Date.now() > exMillis) {
+    return refresh("expired url");
+  }
+
+  return url;
+}
+
+async function refreshAttachmentUrls(urls: string[]) {
+  const token = process.env.DISCORD_CDN_BOT_TOKEN;
+
+  if (token === undefined) {
+    throw new Error(
+      "Cannot refresh attachment urls: env DISCORD_CDN_BOT_TOKEN is not set",
+    );
+  }
+
+  const response = await undici.request(
+    "https://discord.com/api/v10/attachments/refresh-urls",
+    {
+      method: "POST",
+      headers: {
+        authorization: `Bot ${token}`,
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        attachment_urls: urls,
+      }),
+      throwOnError: true,
+    },
+  );
+  const json = (await response.body.json()) as {
+    refreshed_urls: {
+      original: string;
+      refreshed: string;
+    }[];
+  };
+
+  return json.refreshed_urls;
 }
