49431466fe667b1031c771c13bb28334947d0ad8
mdashlw <mdashlw@gmail.com>
2024-06-02 16:40:32 +0300

    feat(booru-selector): auto select only the first booru

--- src/booru-selector.ts
+++ src/booru-selector.ts
@@ -5,10 +5,10 @@ import { boorus } from "./boorus.js";
 export default async function selectBoorus(): Promise<Booru[]> {
   const selectedBoorus = await checkbox({
     message: "Boorus",
-    choices: boorus.map((booru) => ({
+    choices: boorus.map((booru, index) => ({
       value: booru,
       name: booru.name,
-      checked: true,
+      checked: index === 0,
     })),
     required: true,
   });
