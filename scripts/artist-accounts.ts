1575e78cd96d9950b685599f083e6cbe60dbc274
mdashlw <mdashlw@gmail.com>
2024-09-09 19:19:44 +0800

    fix(scripts/artist-accounts): fix Reddit by using Chrome user agent

--- scripts/artist-accounts.ts
+++ scripts/artist-accounts.ts
@@ -304,7 +304,10 @@ const services: Service[] = [
       const response = await undici.request(
         `https://old.reddit.com/user/${encodeURIComponent(name)}/about.json`,
         {
-          headers: { "user-agent": "curl" },
+          headers: {
+            "user-agent":
+              "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+          },
         },
       );
       const data: any = await response.body.json();
