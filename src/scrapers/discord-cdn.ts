9a21a709de86c15419442540c61fc49dcc7d9c42
mdashlw <mdashlw@gmail.com>
2024-12-17 19:10:07 +0800

    feat(discord-cdn): breaking change by Discord: last-modified is no longer accurate

--- src/scrapers/discord-cdn.ts
+++ src/scrapers/discord-cdn.ts
@@ -13,23 +13,12 @@ export async function scrape(url: URL): Promise<SourceData> {
   const cleanUrl = new URL(url);
   cleanUrl.search = "";
 
-  const lastModifiedString = await undici
-    .request(url, {
-      method: "HEAD",
-      throwOnError: true,
-    })
-    .then((response) => response.headers["last-modified"]);
-  if (typeof lastModifiedString !== "string") {
-    throw new Error(`Invalid last-modified header: ${lastModifiedString}`);
-  }
-  const lastModified = new Date(lastModifiedString);
-
   return {
-    source: "Discord",
+    source: null,
     url: cleanUrl.toString(),
     images: [await probeImageUrl(url)],
     artist: null,
-    date: lastModified,
+    date: null,
     title: null,
     description: null,
   };
