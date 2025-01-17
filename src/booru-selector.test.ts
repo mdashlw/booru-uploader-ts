29ee89d038787aec790a63785eb40f056bc358cb
mdashlw <mdashlw@gmail.com>
2025-01-17 19:07:59 +0800

    remove unused files

--- src/booru-selector.test.ts
+++
@@ -1,8 +0,0 @@
-import util from "node:util";
-import selectBoorus from "./booru-selector.ts";
-
-util.inspect.defaultOptions.depth = Infinity;
-
-const boorus = await selectBoorus();
-
-console.log(boorus);
